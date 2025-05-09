7da4abd9e55a073434f8b73b4d0f5e1396fbbf6d
Eduardo Rueda <efrueda-pliego@uh.edu>
2025-05-08 20:15:31 -0500

    made it possible to grab a random movie from my curated list

--- backend/app.py
+++ backend/app.py
@@ -12,8 +12,8 @@ TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 @app.route("/api/movies/random", methods=["GET"])
 def get_random_movie():
     # Use TMDb discover or popular endpoint to fetch movies
-    page = random.randint(1, 500)  # TMDb allows 500 pages max
-    url = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page={page}"
+    page = random.randint(1, 40)  # TMDb allows 500 pages max
+    url = f"https://api.themoviedb.org/3/list/8529848?language=en-US&page={page}"
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer " + TMDB_API_KEY
@@ -21,13 +21,15 @@ def get_random_movie():
     response = requests.get(url, headers=headers)
     data = response.json()
 
-    if "results" in data:
-        movie = random.choice(data["results"])
+    if "items" in data:
+        movie = random.choice(data["items"])
         return jsonify({
             "id": movie["id"],
             "title": movie["title"],
             "poster_path": movie["poster_path"],
-            "overview": movie["overview"]
+            "overview": movie["overview"],
+            "release_date": movie["release_date"],
+            "vote_average": movie["vote_average"]
         })
 
 
