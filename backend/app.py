7d966d75c541479c2bab81f19bdef1d3c19e2dd3
Eduardo Rueda <efrueda-pliego@uh.edu>
2025-05-15 18:11:20 -0500

    successfully grabbed two random movies from my list

--- backend/app.py
+++ backend/app.py
@@ -11,8 +11,8 @@ TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
 @app.route("/api/movies/random", methods=["GET"])
 def get_random_movie():
-    # Use TMDb discover or popular endpoint to fetch movies
-    page = random.randint(1, 40)  # TMDb allows 500 pages max
+    # Use TMDb list details endpoint to fetch movies
+    page = random.randint(1, 40)  # My list has 40 pages
     url = f"https://api.themoviedb.org/3/list/8529848?language=en-US&page={page}"
     headers = {
         "accept": "application/json",
@@ -22,15 +22,59 @@ def get_random_movie():
     data = response.json()
 
     if "items" in data:
-        movie = random.choice(data["items"])
-        return jsonify({
-            "id": movie["id"],
-            "title": movie["title"],
-            "poster_path": movie["poster_path"],
-            "overview": movie["overview"],
-            "release_date": movie["release_date"],
-            "vote_average": movie["vote_average"]
-        })
+        page2 = random.randint(1, 40)  # My list has 40 pages
+        url2 = f"https://api.themoviedb.org/3/list/8529848?language=en-US&page={page2}"
+        headers2 = {
+            "accept": "application/json",
+            "Authorization": "Bearer " + TMDB_API_KEY
+        }
+        response2 = requests.get(url2, headers=headers2)
+        data2 = response2.json()
+        if "items" in data2:
+            movie = random.choice(data["items"])
+            movie2 = random.choice(data2["items"])
+            if movie2["id"] == movie["id"]:
+                page2 = random.randint(1, 40)  # My list has 40 pages
+                url2 = f"https://api.themoviedb.org/3/list/8529848?language=en-US&page={page2}"
+                response2 = requests.get(url2, headers=headers2)
+                data2 = response2.json()
+                return jsonify([
+                {
+                    "id": movie["id"],
+                    "title": movie["title"],
+                    "poster_path": movie["poster_path"],
+                    "overview": movie["overview"],
+                    "release_date": movie["release_date"],
+                    "vote_average": movie["vote_average"]
+                },
+                {
+                    "id": movie2["id"],
+                    "title": movie2["title"],
+                    "poster_path": movie2["poster_path"],
+                    "overview": movie2["overview"],
+                    "release_date": movie2["release_date"],
+                    "vote_average": movie2["vote_average"]
+                },
+                ])
+            else:
+                return jsonify([
+                {
+                    "id": movie["id"],
+                    "title": movie["title"],
+                    "poster_path": movie["poster_path"],
+                    "overview": movie["overview"],
+                    "release_date": movie["release_date"],
+                    "vote_average": movie["vote_average"]
+                },
+                {
+                    "id": movie2["id"],
+                    "title": movie2["title"],
+                    "poster_path": movie2["poster_path"],
+                    "overview": movie2["overview"],
+                    "release_date": movie2["release_date"],
+                    "vote_average": movie2["vote_average"]
+                },
+                ])
 
 
     return jsonify({"error": "Could not fetch movie"}), 500
